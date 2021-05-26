86b58d7baa990eb5df6bb229c0473148f2f05da2
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-05-26 12:18:39 +0200

    add

---
+++ JS/constans/productList.js
@@ -0,0 +1,23 @@
+export const productArray = [
+  {
+    name: "Hike Jackect KS",
+    dicription: "a man hiking",
+    image: "/images/man-hiking-gold.png",
+    id: "1",
+    price: "200",
+  },
+  {
+    name: "Hike Jacket",
+    dicription: " a man hiking",
+    image: "/images/man-hiking-green.png",
+    id: "2",
+    price: "250",
+  },
+  {
+    name: " Skin Jacket",
+    dicription: " a person with skin jacket",
+    image: "/images/skiing-Recovered.png",
+    id: "3",
+    price: "300",
+  },
+];
